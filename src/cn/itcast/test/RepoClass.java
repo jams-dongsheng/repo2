8dd1a18ffe71bd6a097a394b0d2a19252c92bb07
dongsheng <1367872538@qq.com>
2019-08-11 23:34:07 +0800

    修改

--- src/cn/itcast/test/RepoClass.java
+++ src/cn/itcast/test/RepoClass.java
@@ -3,5 +3,7 @@ package cn.itcast.test;
 public class RepoClass {
     public static void main(String[] args) {
         System.out.println("你好java");
+        System.out.println("1111111111111");
     }
+
 }
